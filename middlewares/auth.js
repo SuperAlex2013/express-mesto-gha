ea59738508a75133422752b5f4d23fe6401fab56
SuperAlex2013 <122688147+SuperAlex2013@users.noreply.github.com>
2023-10-08 19:19:18 +0300

    kk

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,16 +6,14 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
 
-  if (!token) {
-    return next(new UnauthorizedError('Необходима авторизация!'));
+  let payload;
+  try {
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'prpZUoYKk3YJ3nhemFHZ');
+  } catch (err) {
+    next(new UnauthorizedError('Необходима авторизация!'));
+    return;
   }
+  req.user = payload;
 
-  jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'prpZUoYKk3YJ3nhemFHZ', (err, payload) => {
-    if (err) {
-      return next(new UnauthorizedError('Необходима авторизация!'));
-    }
-
-    req.user = payload;
-    next();
-  });
+  next();
 };
